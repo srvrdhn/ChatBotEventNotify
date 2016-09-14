c82662b787a332c5fde18c0770b1dc4c678097b0
tejasbadadare <tejasbadadare@gmail.com>
2016-09-13 17:01:04 -0700

    Move credentials into separate file
    Put your credentials in a credentials.json file with the following
    structure:
    
    {
      "user" : {
          "email" : "value",
          "password" : "value"
      }
    }

--- eventnotify/launch.js
+++ eventnotify/launch.js
@@ -2,7 +2,21 @@ var login = require('../index.js');
 var fs = require('fs');
 var assert = require('assert');
 
-login({email: "redacted@gmail.com", password: "redacted"}, function callback (err, api) {
+// Bot login information
+// DO NOT COMMIT credentials.json TO DATABASE
+var conf = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+var credentials = {
+  email: conf.user.email,
+  password: conf.user.password,
+};
+
+/**
+ * Logs into the bot's FB account, provides callback method that parses user
+ * input and sends it to wit.ai for NLP processing.
+ * @param credentials   Array of email, password strings
+ * @param callback      Callback function to execute upon login
+ */
+login(credentials, function callback (err, api) {
     if(err) return console.error(err);
 
     api.setOptions({listenEvents: true});

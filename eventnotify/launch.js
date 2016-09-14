f506f038b95c5a9091759ada7b0a04e2a1d3a98b
tejasbadadare <tejasbadadare@gmail.com>
2016-09-14 01:14:09 -0700

    Add location parsing, improve formatting

--- eventnotify/launch.js
+++ eventnotify/launch.js
@@ -10,6 +10,11 @@ const Wit = require('./witbot.js');
 // sessionId -> {fbid: facebookUserId, context: sessionState}
 const sessions = {};
 
+// Padding constants
+const HEADER = '~~~~~~~~ EventNotify ~~~~~~~\n';
+const FOOTER = '~~~~~~~~~~~~~~~~~~~~~~~~~';
+
+
 /**
 * Returns the session for the specified fbid.
 * If none are found, creates the session and returns it.
@@ -72,7 +77,19 @@ login(credentials, function callback (err, api) {
             // Get message body
             var text = event.body;
 
-            // Run wit.ai when invoked with "EventNotify ..."
+            var display = '';
+
+            /**************************************************************
+             * -------------------- PARSE USER INPUT -------------------- *
+             **************************************************************/
+
+            // Display current events UI upon "EventNotify"
+            if (text.toLowerCase() === "eventnotify") {
+                var retval = HEADER + displayEvents() + "\n" + FOOTER;
+                api.sendMessage(retval, event.threadID);
+            }
+
+            // Run wit.ai upon "EventNotify ..."
             if (text) {
                 var invoked = text.startsWith("EventNotify ");
             }
@@ -94,27 +111,43 @@ login(credentials, function callback (err, api) {
                     // Retrieve event name
                     if (res.hasOwnProperty('event')) {
                         console.log('Event extracted: ' + res.event[0].value);
-                        api.sendMessage('Event extracted: ' + res.event[0].value, event.threadID);
+                        display += 'Event extracted: ' + res.event[0].value + "\n";
                         eventName = res.event[0].value;
                     } else {
                         console.log('No events found.');
                     }
 
+                    // Retrieve location
+                    if (res.hasOwnProperty('location')) {
+                        console.log('Location extracted: ' + res.location[0].value);
+                        display += 'Location extracted: ' + res.location[0].value + "\n";
+                        location = res.location.value;
+                    } else {
+                        console.log('No locations found.');
+                    }
+
+                    // Retrieve local_search_query
+                    if (res.hasOwnProperty('local_search_query')) {
+                        console.log('Local search query extracted: ' + res.local_search_query[0].value);
+                        display += 'Local search query extracted: ' + res.local_search_query[0].value + "\n";
+                        location = res.local_search_query[0].value;
+                    }
+
                     // Retrieve datetime
                     if (res.hasOwnProperty('datetime')) {
                         console.log('Datetimes(s) extracted: ' + res.datetime[0].value);
-                        api.sendMessage('Datetimes(s) extracted: ' + res.datetime[0].value, event.threadID);
+                        display += 'Datetimes(s) extracted: ' + res.datetime[0].value + "\n";
                         date = res.datetime[0].value;
                     } else {
                         console.log('No datetimes found.');
-                    })
+                    }
 
                     // Retrieve participants
                     if (res.hasOwnProperty('contact')) {
                         for (var i = 0; i < res.contact.length; i++) {
                             var c = res.contact[i];
                             console.log('Contact(s) extracted: ' + c.value);
-                            api.sendMessage('Contact(s) extracted: ' + c.value, event.threadID);
+                            display += 'Contact(s) extracted: ' + c.value + "\n";
                             api.getUserID(c.value.substring(1), function(err, data) {
                                 if(err) return callback(err);
                                 // Send the message to the best match (best by Facebook's criteria)
@@ -125,6 +158,9 @@ login(credentials, function callback (err, api) {
                     } else {
                         console.log('No contacts found.');
                     }
+
+                    console.log('returning: ' + HEADER + display + FOOTER);
+                    api.sendMessage(HEADER + display + FOOTER, event.threadID);
                 })
                 .catch(console.error);
             } else {
@@ -138,3 +174,8 @@ login(credentials, function callback (err, api) {
         }
     });
 });
+
+
+function displayEvents() {
+    return "Your group's events go here";
+}
